d71d1ba6e2c042a53911a5eb4295b513975eeb7f
Dillon Chanis <dillonchanis92@gmail.com>
2018-12-09 11:46:52 -0500

    Cleaning up whitespace

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,11 @@ use Illuminate\Http\Request;
 // });
 
 Route::group([
-
     'middleware' => 'api',
     'prefix' => 'auth'
-
 ], function ($router) {
-
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
-
+    Route::get('me', 'AuthController@me');
 });
